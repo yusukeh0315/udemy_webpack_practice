a3234d67cc0914b75054fedca047b471d965a999
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2021-08-20 15:28:12 +0900

    use vue

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,5 +2,13 @@ import './reactApp.jsx'
 import my from './my.js'
 import '../stylesheets/main.scss';
 
+import Vue from 'vue';
+import VueApp from './VueApp.vue'
+
+new Vue({
+    el: '#vue-root',
+    render: (h) => h(VueApp),
+});
+
 console.log('webpack!');
 my();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     mode: 'development',
@@ -13,6 +14,15 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.vue/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'vue-loader'
+                    }
+                ]
+            },
             {
                 test: /\.js/,
                 exclude: /node_modules/,
@@ -81,6 +91,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
             filename: './stylesheets/main.css'
         }),
