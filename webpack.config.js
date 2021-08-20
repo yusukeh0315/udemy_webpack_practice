ec345e89cc2e4b2ec6e01cf23f42cc6215c5afbc
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2021-08-20 09:58:39 +0900

    use partial template

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,21 @@ module.exports = {
                 //         }
                 //     },
                 // ]
+            },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            //  非圧縮で出力する場合のオプション
+                            pretty: true,
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -45,7 +60,12 @@ module.exports = {
             filename: './stylesheets/main.css'
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html'
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin()
     ]
