024ff703becfceccd7349f15e9a181ba41aaea00
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2021-08-20 09:03:57 +0900

    use asset modules

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,19 @@ module.exports = {
             },
             {
                 test: /\.png|\.jpg/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            esModule: false,
-                            name: 'images/[name].[ext]'
-                        }
-                    },
-                ]
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+                // use: [
+                //     {
+                //         loader: 'file-loader',
+                //         options: {
+                //             esModule: false,
+                //             name: 'images/[name].[ext]'
+                //         }
+                //     },
+                // ]
             }
         ]
     },
